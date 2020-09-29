ae515f733639f0978e225fa4c0d76beb222b5152
Mukul <mukulpahwa504@gmmail.com>
2020-09-29 23:06:46 +0530

    change update learn goal

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,25 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello!</p>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Learn <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >React </a> and  <a
+            className="App-link"
+            href="https://redux-saga.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          > redux-saga </a> and  <a
+            className="App-link"
+            href="https://www.arduino.cc/"
+            target="_blank"
+            rel="noopener noreferrer"
+          > arduino </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
